da7e29d3793b6a7f26b5d1ab83ced563ebd3d8ba
mranha0 <67779111+mranha0@users.noreply.github.com>
2024-02-21 15:12:02 +0900

    updates

--- research/engine/js/vlbdata1.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:39,classNames:'vlightbox1',descSliding:true,enableRightClick:true,enableSlideshow:true,resizeSpeed:8,slideTime:3,startZoom:true}) });
